b4eaecec5fee7767bcdf25b2462a1aeb1a870b13
HuntingRathalos <huntingandrathalos@gmail.com>
2023-03-22 21:56:05 +0900

    過去の投稿画面

---
+++ php/views/topic/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <div id="global-container">
+            <div id="container">
+                <main class="container py-3">
+                    <h1 class="h2 mb-3">過去の投稿</h1>
+                        <ul class="container">
+                            <li class="row bg-white shadow-sm mb-3 rounded p-3">
+                                <div class="col-md">
+                                    <h3>
+                                        <span class="badge">公開</span>
+                                        <a>犬も歩けば棒に当たりますか？</a>
+                                    </h3>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="text-center row">
+                                        <div class="view">
+                                            <div class="h1 mb-0">26</div>
+                                            <div class="mb-0">Views</div>
+                                        </div>
+                                        <div class="agree">
+                                            <div class="h1 mb-0">26</div>
+                                            <div class="mb-0">賛成</div>
+                                        </div>
+                                        <div class="disagree">
+                                            <div class="h1 mb-0">26</div>
+                                            <div class="mb-0">反対</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                </main>
+            </div>
+        </div>
+    </body>
+</html>
