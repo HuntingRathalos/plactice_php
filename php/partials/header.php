cc8aaa94a32d075f2ac1e432672d72a478b92f49
HuntingRathalos <huntingandrathalos@gmail.com>
2023-03-28 23:05:51 +0900

    headerの修正

--- php/partials/header.php
+++ php/partials/header.php
@@ -16,26 +16,27 @@
 
 <body>
 <div id="container">
-                <header class="container my-2">
-                    <nav class="row align-items-center py-2">
-                        <a href="<?php the_url('/');?>"
-                            class="col-md d-flex align-items-center mb-3 mb-md-0"
-                        >
-                            <img width="50" class="mr-2" src="" alt="" />
-                            <span class="h2 font-weight-bold mb-0"
-                                >みんなのアンケート</span
-                            >
-                        </a>
-                        <div class="col-md-auto">
-                            <?php if(true) :  ?>
-                            <a href="<?php the_url('register');?>" class="btn btn-primary mr-2">登録</a>
-                            <a href="<?php the_url('register');?>">ログイン</a>
-                            <?php else : ?>
-                                <!-- ログインしている時と出し分ける -->
-                            <?php endif ; ?>
-                        </div>
-                    </nav>
-                </header>
+    <header class="container my-2">
+        <nav class="row align-items-center py-2">
+            <a href="<?php the_url('/');?>"
+                class="col-md d-flex align-items-center mb-3 mb-md-0"
+            >
+                <img width="50" class="mr-2" src="" alt="" />
+                <span class="h2 font-weight-bold mb-0"
+                    >みんなのアンケート</span
+                >
+            </a>
+            <div class="col-md-auto">
+                <?php if(true) :  ?>
+                <a href="<?php the_url('register');?>" class="btn btn-primary mr-2">登録</a>
+                <a href="<?php the_url('register');?>">ログイン</a>
+                <?php else : ?>
+                    <!-- ログインしている時と出し分ける -->
+                <?php endif ; ?>
+            </div>
+        </nav>
+    </header>
+    <main class="container py-3">
     <?php
                 Msg::flush();
     ?>
