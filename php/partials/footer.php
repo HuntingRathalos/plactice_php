6c8d571e6658e6f0d0079b317a2d30b4128ec757
HuntingRathalos <huntingandrathalos@gmail.com>
2023-03-28 23:09:49 +0900

    footerの移植

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -1,4 +1,9 @@
- <!-- <script src="<?php echo BASE_JS_PATH ?>sample.js"></script> -->
- </body>
-
- </html>
+<!-- <script src="<?php echo BASE_JS_PATH ?>sample.js"></script> -->
+            </main>
+            <footer class="text-center p-3">
+                    <span class="text-muted">&copy; ChubbyMan</span>
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
