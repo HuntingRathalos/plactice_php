ab2ede534ce52ffeed0f9b45e104e36cdedfecc3
HuntingRathalos <huntingandrathalos@gmail.com>
2023-04-04 19:32:11 +0900

    トピック一覧画面仮実装

--- php/controllers/topic/archive.php
+++ php/controllers/topic/archive.php
@@ -9,8 +9,11 @@ use lib\Auth;
 function get()
 {
     Auth::requireLogin();
+
     $user = UserModel::getSession();
     $topics = TopicQuery::fetchByUserId($user);
+
+    \view\topic\archive\index($topics);
 }
 
 function post()
