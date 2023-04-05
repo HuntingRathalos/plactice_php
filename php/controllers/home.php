48d632fb5eab9ab3d6841700e785a2f861017d35
HuntingRathalos <huntingandrathalos@gmail.com>
2023-04-06 04:04:53 +0900

    topic-header-item実装

--- php/controllers/home.php
+++ php/controllers/home.php
@@ -2,9 +2,12 @@
 
 namespace controllers\home;
 
+use db\TopicQuery;
+
 function get()
 {
     // require_once SOURCE_BASE . 'views/home.php';
+    $topics = TopicQuery::fetchPublishedTopics();
     \view\home\index($topics);
 }
 
