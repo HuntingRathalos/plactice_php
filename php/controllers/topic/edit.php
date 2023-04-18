b00af7e4e68d743259c76343fa55cc33775b8be8
HuntingRathalos <huntingandrathalos@gmail.com>
2023-04-18 14:51:13 +0900

    editController postメソッド実装

--- php/controllers/topic/edit.php
+++ php/controllers/topic/edit.php
@@ -4,6 +4,7 @@ namespace controllers\topic\edit;
 
 use db\TopicQuery;
 use lib\Auth;
+use lib\Msg;
 use models\TopicModel;
 use models\UserModel;
 use Throwable;
@@ -21,4 +22,32 @@ function get()
     $fetchedTopic = TopicQuery::fetchById($topic);
 
     \view\topic\edit\index($fetchedTopic);
+}
+
+function post()
+{
+    Auth::requireLogin();
+
+    $topic = new TopicModel();
+    $topic->id = get_param('topic_id', null);
+    $topic->title = get_param('title', null);
+    $topic->published = get_param('published', null);
+
+    $user = UserModel::getSession();
+    Auth::requirePermission($topic->id, $user);
+
+    try {
+        $is_success = TopicQuery::update($topic);
+    } catch(Throwable $e) {
+        Msg::push(Msg::DEBUG, $e->getMessage());
+        $is_success = false;
+    }
+
+    if ($is_success) {
+        Msg::push(Msg::INFO, 'トピックの更新に成功しました。');
+        redirect('topic/archive');
+    } else {
+        Msg::push(Msg::ERROR, 'トピックの更新に失敗しました。');
+        redirect('GO_REFFERER');
+    }
 }
